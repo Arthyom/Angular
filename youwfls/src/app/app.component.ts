d50498ac84eb36361842b0f8d4d4be4de0478ebc
arthyom <a.gonzalezgavina@ugto.mx>
2020-05-01 04:55:10 -0500

    youtube

---
+++ youwfls/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewChild } from '@angular/core';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'youwfls';
+}
+
+
---
+++ youwfls/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './componentes/home/home.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    MatMenuModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
